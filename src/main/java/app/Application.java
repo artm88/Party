96c13732f633754bd91b208b603be8c13358c7f0
artm88 <akoryukin@gmail.com>
2022-10-28 23:04:06 +0300

    +realization of exit game

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -14,13 +14,14 @@ public class Application {
         Registrator registrator = new RegistratorImpl();
         User user = registrator.registerUser();
         Game game = new GameImpl(user);
+        int exit=0;
         do {
             game.play();
             System.out.println(user.getName() + ", твой счет игры " + user.scoreOfGame());
-            System.out.println("Нажми цифру 4 для выхода");
+            System.out.println("Нажми цифру 4 для выхода или любую другую цифру для продолжения");
             Scanner scannerOfExit = new Scanner(System.in); // взаимодействие с пользователем
-            if (scannerOfExit.equals("4")) { return true;}
-        } while (false );
+            exit=scannerOfExit.nextInt();
+        } while (exit!=4);
     }
 
 }
--- src/main/java/impl/GameImpl.java
+++ src/main/java/impl/GameImpl.java
@@ -16,16 +16,17 @@ public class GameImpl implements Game {
     }
     @Override
     public void play() {
-        System.out.println("Сделайте выбор (введите номер выбора): " +
-                "/n 1. Выишрать /n 2. Проиграть /n 3. Пропуск /n 4. Выход /n");
+        System.out.print("Сделайте выбор (введите номер выбора): " +
+                "\n 1. Выишрать \n 2. Проиграть \n 3. Пропуск \n 4. Выход \n");
         Scanner scanner = new Scanner(System.in); // взаимодействие с пользователем
-        if (scanner.equals("1")) {
-            user.reduceScore(1);
-        } else if (scanner.equals("2")){
+        int temp = scanner.nextInt();
+        if (temp==1) {
             user.increaseScore(1);
-        } else if (scanner.equals("3")){
+        } else if (temp==2){
+            user.reduceScore(1);
+        } else if (temp==3){
             System.out.println("Пропуск");
-        } else if (scanner.equals("4")){
+        } else if (temp==4){
             System.out.println("Выход");
         }
     }
