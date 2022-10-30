6cebdc8beddd4a280efda310c3eaf9419465b1af
artm88 <akoryukin@gmail.com>
2022-10-30 22:19:08 +0300

    +realization of random generation of object for procedure of choice

--- src/main/java/impl/GameImpl.java
+++ src/main/java/impl/GameImpl.java
@@ -3,6 +3,7 @@ package impl;
 import api.Game;
 import api.User;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class GameImpl implements Game {
@@ -16,17 +17,25 @@ public class GameImpl implements Game {
     }
     @Override
     public void play() {
-        System.out.print("Сделайте выбор (введите номер выбора): " +
-                "\n 1. Выишрать \n 2. Проиграть \n 3. Пропуск \n 4. Выход \n");
+        Random random = new Random();
+        int objectOne = 1+random.nextInt(2); // генерация первого объекта комнаты
+        int objectTwo = 1+random.nextInt(2); // генерация второго объекта комнаты
+        System.out.print("Сделайте выбор (введите номер выбора), при условии, что 1 приведет к выигрошу, а 2 к поражению:" +
+                "\n [1] "+objectOne+" \n [2] " +objectTwo+" \n [3] Пропуск \n [4] Выход \n");
         Scanner scanner = new Scanner(System.in); // взаимодействие с пользователем
         int temp = scanner.nextInt();
-        if (temp==1) {
-            user.increaseScore(1);
-        } else if (temp==2){
-            user.reduceScore(1);
-        } else if (temp==3){
+        if (temp==1) { if (objectOne==1) {  // если игрок выбирает 1 сюжетную ветку
+            user.increaseScore(1);}
+            else if (objectOne==2) {
+            user.reduceScore(1);}
+        } else if (temp==2){ // если игрок выбирает 2 сюжетную ветку
+            if (objectTwo==1) {
+                user.increaseScore(1);}
+            else if (objectTwo==2) {
+                user.reduceScore(1);}
+        } else if (temp==3){ // если игрок выбирает пропуск хода
             System.out.println("Пропуск");
-        } else if (temp==4){
+        } else if (temp==4){ // если игрок выбирает выйти
             System.out.println("Выход");
         }
     }
