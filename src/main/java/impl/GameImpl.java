25fda7b7959f36fd7848f38bc4c803b5066573c7
artm88 <akoryukin@gmail.com>
2022-10-31 14:02:38 +0300

    +add environment

--- src/main/java/impl/GameImpl.java
+++ src/main/java/impl/GameImpl.java
@@ -11,6 +11,7 @@ public class GameImpl implements Game {
     //private   COUNT_OF_WIN   int
     //private   LIST_OF_PEOPLE   коллекция или БД
     //private   LIST_OF_OBJECT    коллекция или БД
+    private String[] LIST_OF_ENVIRONMENT={"Человек", "Человек с книгой", "Люди", "кресло", "шкаф", "угол"};
     private final User user;
     public GameImpl(User user) {
         this.user = user;
@@ -18,37 +19,29 @@ public class GameImpl implements Game {
     @Override
     public void play() {
         Random random = new Random();
-        int objectOne = 1+random.nextInt(2); // генерация первого объекта комнаты
-        int objectTwo = 1+random.nextInt(2); // генерация второго объекта комнаты
-        String StringOne = new String(); // cвязь сюжетного выбора и типа объекта
-        if (objectOne==1) {
-            StringOne = "Человек";}
-        else if (objectOne==2) {
-            StringOne = "предмет";}
-        String StringTwo = new String();
-        if (objectTwo==1) {
-            StringTwo = "Человек";}
-        else if (objectTwo==2) {
-            StringTwo = "предмет";}
+        int objectOne = random.nextInt(6); // генерация первого объекта комнаты, 6 - число объектов
+        int objectTwo = random.nextInt(6); // генерация второго объекта комнаты
+        String StringOne = LIST_OF_ENVIRONMENT[objectOne]; // cвязь сюжетного выбора и типа объекта
+        String StringTwo = LIST_OF_ENVIRONMENT[objectTwo];
         System.out.print("Сделайте выбор (введите номер выбора):" +
-                "\n [1] "+StringOne+" \n [2] " +StringTwo+" \n [3] Пропуск \n [4] Устал \n");
+                "\n [1] "+StringOne+" \n [2] " +StringTwo+" \n [3] Не хочу решать \n [4] Отдохнуть \n");
         Scanner scanner = new Scanner(System.in); // взаимодействие с пользователем
         int temp = scanner.nextInt();
-        if (objectOne==1||objectTwo==1){user.increaseMaxScore(1);}
-        if (temp==1) { if (objectOne==1) {  // если игрок выбирает 1 сюжетную ветку
+        if (objectOne<3||objectTwo<3){user.increaseMaxScore(1);}
+        if (temp==1) { if (objectOne<3) {  // если игрок выбирает 1 сюжетную ветку
             user.increaseScore(1);}
-            else if (objectOne==2) {
+            else if (objectOne>=3) {
             user.reduceScore(1);}
         } else if (temp==2){ // если игрок выбирает 2 сюжетную ветку
-            if (objectTwo==1) {
+            if (objectTwo<3) {
                 user.increaseScore(1);}
-            else if (objectTwo==2) {
+            else if (objectTwo>=3) {
                 user.reduceScore(1);}
-        } else if (temp==3){ // если игрок выбирает пропуск хода
+        } else if (temp==3){ // если игрок выбирает пропуск хода, считается за проигрыш
             System.out.println("Пропуск");
-        } else if (temp==4){ // если игрок устал
+        } else if (temp==4){ // если игрок устал, ход не пишется в общий счёт
             System.out.println("Устал");
-            if (objectOne==1||objectTwo==1){user.reduceMaxScore(1);}
+            if (objectOne<3||objectTwo<3){user.reduceMaxScore(1);}
         }
     }
 }
