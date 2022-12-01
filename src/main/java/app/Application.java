aac49d6636ca35fcd403a3954a35f154bc1348c0
artm88 <akoryukin@gmail.com>
2022-12-01 16:41:32 +0300

    +db postgres

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -10,6 +10,7 @@ import impl.RegistratorImpl;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Application {
@@ -38,29 +39,15 @@ public class Application {
             exit=scannerOfExit.nextInt();
         } while (exit!=5);
 
-        // результат и статистика игрока
-        if (user.scoreOfGame()>user.maxScore()/2)   {
-        System.out.println(user.getName() + ", Вы социально адаптированы." );}
-        else {System.out.println(user.getName() + ", Вы стараетесь избегать людей." );}
-        System.out.println("Нажмите цифру 6 для вывода статистики"); // развилка на статистику
-        Scanner scannerOfStat = new Scanner(System.in);
-        int stat=0;
-        stat=scannerOfStat.nextInt();
-        if (stat==6) {
-            System.out.println(user.getName() + ", Вы имеете " + user.scoreOfGame() +
-                    " баллов выбора людей из максимально возможных " + user.maxScore());
-            System.out.println("Баллов социальной адаптации " + Math.round(10*user.scoreOfGame()/ user.maxScore()) +
-                    " по 10-бальной шкале.");
-        }
-        else {}
-
         // запись статистики игрока
         String STATISTIC = "C:\\Users\\artem\\IdeaProjects\\Party\\src\\main\\resources\\"+user.getName()+"_statistic.txt";
         try (PrintWriter printWriterSTATISTIC = new PrintWriter(new FileOutputStream(STATISTIC))) {
-            printWriterSTATISTIC.printf(user.getName() + " имеет " + user.scoreOfGame() +
-                    " баллов выбора людей из максимально возможных " + user.maxScore()+
-                    ".\nБаллов социальной адаптации " + Math.round(10*user.scoreOfGame()/ user.maxScore()) +
-                    " по 10-бальной шкале.\n");
+            if (user.maxScore()!=0) {
+                printWriterSTATISTIC.printf(user.getName() + " имеет " + user.scoreOfGame() +
+                        " баллов выбора людей из максимально возможных " + user.maxScore() +
+                        ".\nБаллов социальной адаптации " + Math.round(10 * user.scoreOfGame() / user.maxScore()) +
+                        " по 10-бальной шкале.\n");
+            }
         } catch (FileNotFoundException ex){
             System.out.println("Не найден Файл!");
         }
@@ -68,24 +55,46 @@ public class Application {
         printWriterLOGS.printf("Игра закончена.\n"); //логи
         printWriterLOGS.close();
 
+        // результат и статистика игрока
+        if (user.scoreOfGame()>user.maxScore()/2)   {
+        System.out.println(user.getName() + ", Вы социально адаптированы." );}
+        else {System.out.println(user.getName() + ", Вы стараетесь избегать людей." );}
+        System.out.println("Нажмите цифру 6 для вывода статистики или любую другую цифру для выхода"); // развилка на статистику
+        Scanner scannerOfStat = new Scanner(System.in);
+        int stat=0;
+        stat=scannerOfStat.nextInt();
+        if (stat!=6) {
+        }
+        else {
+            System.out.println(user.getName() + ", Вы имеете " + user.scoreOfGame() +
+                    " баллов выбора людей из максимально возможных " + user.maxScore());
+            if (user.maxScore()!=0) {
+                System.out.println("Баллов социальной адаптации " + Math.round(10 * user.scoreOfGame() / user.maxScore()) +
+                        " по 10-бальной шкале.");
+            }
+        }
+        System.out.println("Спасибо! \nИгру делал Корюкин Артём.");
+
         // запись имение и счета  игрока в базу данных
         // еще одну БД сделать для автосохранений  и сохранений во время игры
-        /*String url = "jdbc:postgresql://localhost:5432/postgres";
+        String url = "jdbc:postgresql://localhost:5432/postgres";
         String userBD="postgres";
         String passwordDb="artem";
-        String query="insert into public.accounts (user_id, username, score) VALUES(?, ?, ?)";
-        try (Connection connectionFinal= DriverManager.getConnection(url, userBD, passwordDb);
-             PreparedStatement preparedStatementFinal = connectionFinal.prepareStatement(query)) {
-            preparedStatementFinal.setInt(1,2);
-            ResultSet resultSetFinal=preparedStatementFinal.executeQuery();
-            while (resultSetFinal.next()){
-                int user_id = resultSetFinal.getInt("user_id");
-                String username=resultSetFinal.getString("username");
-                int score = resultSetFinal.getInt("score");
+        //String query="insert into public.accounts (username, score) VALUES(?, ?)";
+        String query="select * from public.accounts";
+        try (Connection connection= DriverManager.getConnection(url, userBD, passwordDb);
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            //preparedStatement.setInt(1,2);
+            ResultSet resultSet=preparedStatement.executeQuery();
+            while (resultSet.next()){
+                int user_id = resultSet.getInt("user_id");
+                String username=resultSet.getString("username");
+                int score = resultSet.getInt("score");
+                System.out.println(" Пациент "+ username +" : "+ score + " баллов.");
             }
         } catch (SQLException excpt){
             throw new RuntimeException(excpt);
-        }*/
+        }
     }
 
 }
