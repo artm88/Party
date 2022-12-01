d856af88a2150aa473356158378af7f2ea7c614e
artm88 <akoryukin@gmail.com>
2022-12-01 22:37:51 +0300

    +db with data

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -26,7 +26,8 @@ public class Application {
         printWriterLOGS.printf("\nЗагружена игра.\n");//логи
         int exit=0;
         System.out.println("Добро пожаловать на коктейльную пати в честь вашего устройства на работу.\n" +
-                "Вы в комнате, перед Вами выбор куда подойти. Так же Вы можете пройти мимо или отдохнуть.\n"); // главное меню
+                "Вы в комнате, перед Вами выбор куда подойти. " +
+                "Так же Вы можете пройти мимо или отдохнуть.\n"); // главное меню
 
         //  сама игра
         do {
@@ -34,18 +35,25 @@ public class Application {
             printWriterLOGS.printf("Пройден уровень игры.\n");//логи
             //System.out.println(user.getName() + ", твой счет игры " + user.scoreOfGame()+
                   //  " из максимально возможных "+user.maxScore()); // отображение счета игры (временная опция)
-            System.out.println("Нажмите цифру 5 для выхода или любую другую цифру для продолжения"); // развилка на выход
+            System.out.println("Нажмите цифру 5 для выхода или любую другую цифру для продолжения"); //развилка на выход
             Scanner scannerOfExit = new Scanner(System.in); // взаимодействие с пользователем
             exit=scannerOfExit.nextInt();
         } while (exit!=5);
 
         // запись статистики игрока
-        String STATISTIC = "C:\\Users\\artem\\IdeaProjects\\Party\\src\\main\\resources\\"+user.getName()+"_statistic.txt";
+        String STATISTIC = "C:\\Users\\artem\\IdeaProjects\\Party\\src\\main\\resources\\"+user.getName()+
+                "_statistic.txt";
         try (PrintWriter printWriterSTATISTIC = new PrintWriter(new FileOutputStream(STATISTIC))) {
             if (user.maxScore()!=0) {
                 printWriterSTATISTIC.printf(user.getName() + " имеет " + user.scoreOfGame() +
                         " баллов выбора людей из максимально возможных " + user.maxScore() +
                         ".\nБаллов социальной адаптации " + Math.round(10 * user.scoreOfGame() / user.maxScore()) +
+                        " по 10-бальной шкале.\n");
+            }
+            else {
+                printWriterSTATISTIC.printf(user.getName() + " имеет " + user.scoreOfGame() +
+                        " баллов выбора людей из максимально возможных " + user.maxScore() +
+                        ".\nБаллов социальной адаптации " +  user.maxScore() +
                         " по 10-бальной шкале.\n");
             }
         } catch (FileNotFoundException ex){
@@ -59,7 +67,8 @@ public class Application {
         if (user.scoreOfGame()>user.maxScore()/2)   {
         System.out.println(user.getName() + ", Вы социально адаптированы." );}
         else {System.out.println(user.getName() + ", Вы стараетесь избегать людей." );}
-        System.out.println("Нажмите цифру 6 для вывода статистики или любую другую цифру для выхода"); // развилка на статистику
+        System.out.println("Нажмите цифру 6 для вывода статистики " +
+                "или любую другую цифру для выхода"); // развилка на статистику
         Scanner scannerOfStat = new Scanner(System.in);
         int stat=0;
         stat=scannerOfStat.nextInt();
@@ -69,32 +78,99 @@ public class Application {
             System.out.println(user.getName() + ", Вы имеете " + user.scoreOfGame() +
                     " баллов выбора людей из максимально возможных " + user.maxScore());
             if (user.maxScore()!=0) {
-                System.out.println("Баллов социальной адаптации " + Math.round(10 * user.scoreOfGame() / user.maxScore()) +
+                System.out.println("Баллов социальной адаптации " +
+                        Math.round(10 * user.scoreOfGame() / user.maxScore()) +
                         " по 10-бальной шкале.");
+            }
+            else{
+                System.out.println("Баллов социальной адаптации " +
+                        user.maxScore() + " по 10-бальной шкале.");
             }
         }
         System.out.println("Спасибо! \nИгру делал Корюкин Артём.");
 
         // запись имение и счета  игрока в базу данных
-        // еще одну БД сделать для автосохранений  и сохранений во время игры
-        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String url = "jdbc:postgresql://localhost:5432/userscore";
         String userBD="postgres";
         String passwordDb="artem";
-        //String query="insert into public.accounts (username, score) VALUES(?, ?)";
-        String query="select * from public.accounts";
-        try (Connection connection= DriverManager.getConnection(url, userBD, passwordDb);
-             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+        //try (Connection connectionForCreateTable= DriverManager.getConnection(url, userBD, passwordDb);
+              // Statement statement = connectionForCreateTable.createStatement()) {
+         //   statement.execute("DROP TABLE IF EXISTS accounts");
+          //  statement.execute("CREATE TABLE accounts (" +
+           //         " user_id serial PRIMARY KEY," +
+            //        " username VARCHAR (50) UNIQUE NOT NULL,"+
+          //          " score serial,"+
+            //        " scoreMax serial"+
+             //       ");");
+        //}
+        //catch (SQLException excpt){
+           // throw new RuntimeException(excpt);
+        //}
+        int userid=0;
+        String queryForId="select user_id from accountsUserScore";
+        try (Connection connectionForId= DriverManager.getConnection(url, userBD, passwordDb);
+             PreparedStatement StatementForId = connectionForId.prepareStatement(queryForId)) {
             //preparedStatement.setInt(1,2);
-            ResultSet resultSet=preparedStatement.executeQuery();
+            ResultSet resultSet=StatementForId.executeQuery();
+            while (resultSet.next()){
+            userid = resultSet.getInt("user_id");
+            }
+        }
+        catch (SQLException excpt){
+            throw new RuntimeException(excpt);
+        }
+
+        String query="INSERT INTO accountsUserScore(user_id, username, score, scoreMax) VALUES (?, ?, ?, ?)";
+        try (Connection connectionForCreateData= DriverManager.getConnection(url, userBD, passwordDb);
+             PreparedStatement preparedStatement = connectionForCreateData.prepareStatement(query))
+        {
+            preparedStatement.setInt(1, userid+1);
+            preparedStatement.setString(2, user.getName());
+            preparedStatement.setInt(3, user.scoreOfGame());
+            preparedStatement.setInt(4, user.maxScore());
+            preparedStatement.execute();
+        }
+        catch (SQLException excpt){
+            throw new RuntimeException(excpt);
+        }
+
+        String queryForTest="select * from accountsUserScore";
+        try (Connection connectionForTest= DriverManager.getConnection(url, userBD, passwordDb);
+             PreparedStatement StatementForTest = connectionForTest.prepareStatement(queryForTest)) {
+            //preparedStatement.setInt(1,2);
+            ResultSet resultSet=StatementForTest.executeQuery();
             while (resultSet.next()){
                 int user_id = resultSet.getInt("user_id");
                 String username=resultSet.getString("username");
                 int score = resultSet.getInt("score");
-                System.out.println(" Пациент "+ username +" : "+ score + " баллов.");
+                int scoreMax = resultSet.getInt("scoreMax");
+                System.out.println("Пациент № " + user_id +". "+ username +" имеет "+ score +
+                        " баллов из "+ scoreMax+" социальной адаптации.");
             }
         } catch (SQLException excpt){
             throw new RuntimeException(excpt);
         }
+
+        // запись имение и счета  игрока в базу данных
+        // еще одну БД сделать для автосохранений  и сохранений во время игры
+        //String url = "jdbc:postgresql://localhost:5432/postgres";
+        //String userBD="postgres";
+        //String passwordDb="artem";
+        //String query="insert into public.accounts (username, score) VALUES(?, ?)";
+        //String query="select * from public.accounts";
+        //try (Connection connection= DriverManager.getConnection(url, userBD, passwordDb);
+        //  PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            //preparedStatement.setInt(1,2);
+            //ResultSet resultSet=preparedStatement.executeQuery();
+            //while (resultSet.next()){
+          //  int user_id = resultSet.getInt("user_id");
+        //    String username=resultSet.getString("username");
+        //    int score = resultSet.getInt("score");
+        //    System.out.println(" Пациент "+ username +" : "+ score + " баллов.");
+        //    }
+        //} catch (SQLException excpt){
+        //    throw new RuntimeException(excpt);
+        //}
     }
 
 }
--- src/main/java/impl/GameImpl.java
+++ src/main/java/impl/GameImpl.java
@@ -33,24 +33,29 @@ public class GameImpl implements Game {
         String StringOne = L_O_ENVIRONMENTS.get(objectOne); // cвязь сюжетного выбора и типа объекта
         String StringTwo = L_O_ENVIRONMENTS.get(objectTwo);
         System.out.print("Сделайте выбор (введите номер выбора):" +
-                "\n [1] Подойти к "+StringOne+" \n [2] Подойти к " +StringTwo+" \n [3] Пройти дальше \n [4] Отдохнуть \n");
+                "\n [1] Подойти к "+StringOne+" \n [2] Подойти к " +
+                StringTwo+" \n [3] Пройти дальше \n [4] Отдохнуть \n");
 
         //основная часть игры
         Scanner scanner = new Scanner(System.in); // взаимодействие с пользователем
         int temp = scanner.nextInt();
         if (objectOne<3||objectTwo<3){user.increaseMaxScore(1);}
-        if (temp==1) { if (objectOne<3) {  // если игрок выбирает 1 сюжетную ветку
-            user.increaseScore(1);}
+        if (temp==1) {
+            if (objectOne<3) {  // если игрок выбирает 1 сюжетную ветку
+                user.increaseScore(1);}
             else if (objectOne>=3) {
             user.reduceScore(1);}
-        } else if (temp==2){ // если игрок выбирает 2 сюжетную ветку
+        }
+        else if (temp==2){ // если игрок выбирает 2 сюжетную ветку
             if (objectTwo<3) {
                 user.increaseScore(1);}
             else if (objectTwo>=3) {
                 user.reduceScore(1);}
-        } else if (temp==3){ // если игрок выбирает пропуск хода, считается за проигрыш в случае наличия человек в выборе
+        }
+        else if (temp==3){ //если игрок выбирает пропуск хода, считается за проигрыш в случае наличия человек в выборе
             System.out.println("Проходите дальше...");
-        } else if (temp==4){ // если игрок выбирает отдых, то ход не пишется в общий счёт
+        }
+        else if (temp==4){ // если игрок выбирает отдых, то ход не пишется в общий счёт
             System.out.println("Отдыхаете...");
             if (objectOne<3||objectTwo<3){user.reduceMaxScore(1);}
         }
