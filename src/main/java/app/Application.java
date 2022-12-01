f00ad365d0e8228e6a4034f759fc6bdb14287f2c
artm88 <akoryukin@gmail.com>
2022-12-01 23:13:21 +0300

    +add pdf

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -29,6 +29,8 @@ public class Application {
                 "Вы в комнате, перед Вами выбор куда подойти. " +
                 "Так же Вы можете пройти мимо или отдохнуть.\n"); // главное меню
 
+        //
+
         //  сама игра
         do {
             game.play();
@@ -89,10 +91,13 @@ public class Application {
         }
         System.out.println("Спасибо! \nИгру делал Корюкин Артём.");
 
-        // запись имение и счета  игрока в базу данных
+        // запись имени и счета  игрока в базу данных
+
         String url = "jdbc:postgresql://localhost:5432/userscore";
         String userBD="postgres";
         String passwordDb="artem";
+
+        // запаска для создание базы
         //try (Connection connectionForCreateTable= DriverManager.getConnection(url, userBD, passwordDb);
               // Statement statement = connectionForCreateTable.createStatement()) {
          //   statement.execute("DROP TABLE IF EXISTS accounts");
@@ -106,6 +111,8 @@ public class Application {
         //catch (SQLException excpt){
            // throw new RuntimeException(excpt);
         //}
+
+        // подгрузка последнего id для увеличение его значение на 1
         int userid=0;
         String queryForId="select user_id from accountsUserScore";
         try (Connection connectionForId= DriverManager.getConnection(url, userBD, passwordDb);
@@ -120,6 +127,7 @@ public class Application {
             throw new RuntimeException(excpt);
         }
 
+        // запись новой строки - данных нового пользователя
         String query="INSERT INTO accountsUserScore(user_id, username, score, scoreMax) VALUES (?, ?, ?, ?)";
         try (Connection connectionForCreateData= DriverManager.getConnection(url, userBD, passwordDb);
              PreparedStatement preparedStatement = connectionForCreateData.prepareStatement(query))
@@ -134,6 +142,7 @@ public class Application {
             throw new RuntimeException(excpt);
         }
 
+        // отображение базы данных
         String queryForTest="select * from accountsUserScore";
         try (Connection connectionForTest= DriverManager.getConnection(url, userBD, passwordDb);
              PreparedStatement StatementForTest = connectionForTest.prepareStatement(queryForTest)) {
@@ -150,27 +159,6 @@ public class Application {
         } catch (SQLException excpt){
             throw new RuntimeException(excpt);
         }
-
-        // запись имение и счета  игрока в базу данных
-        // еще одну БД сделать для автосохранений  и сохранений во время игры
-        //String url = "jdbc:postgresql://localhost:5432/postgres";
-        //String userBD="postgres";
-        //String passwordDb="artem";
-        //String query="insert into public.accounts (username, score) VALUES(?, ?)";
-        //String query="select * from public.accounts";
-        //try (Connection connection= DriverManager.getConnection(url, userBD, passwordDb);
-        //  PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            //preparedStatement.setInt(1,2);
-            //ResultSet resultSet=preparedStatement.executeQuery();
-            //while (resultSet.next()){
-          //  int user_id = resultSet.getInt("user_id");
-        //    String username=resultSet.getString("username");
-        //    int score = resultSet.getInt("score");
-        //    System.out.println(" Пациент "+ username +" : "+ score + " баллов.");
-        //    }
-        //} catch (SQLException excpt){
-        //    throw new RuntimeException(excpt);
-        //}
     }
 
 }
