b1e802b643864b932d2dea7f0412ad9eae8850dc
artm88 <akoryukin@gmail.com>
2022-12-02 12:33:51 +0300

    +add load user and ask name

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -15,21 +15,64 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) throws FileNotFoundException {
-
-        Registrator registrator = new RegistratorImpl();
-        User user = registrator.registerUser();
-        String LOGS = "C:\\Users\\artem\\IdeaProjects\\Party\\src\\main\\resources\\"+user.getName()+"_log.txt";//логи
-        PrintWriter printWriterLOGS = new PrintWriter(new FileOutputStream(LOGS));
-        printWriterLOGS.printf("Регистратция пройдена. Добавлено имя пользователя: "+ user.getName()+
-                ". Введен счет пользователя: "+user.scoreOfGame()+". Введен максимальный счет теста: "+user.maxScore());
-        Game game = new GameImpl(user);
-        printWriterLOGS.printf("\nЗагружена игра.\n");//логи
-        int exit=0;
-        System.out.println("Добро пожаловать на коктейльную пати в честь вашего устройства на работу.\n" +
-                "Вы в комнате, перед Вами выбор куда подойти. " +
-                "Так же Вы можете пройти мимо или отдохнуть.\n"); // главное меню
-
-        //
+        int load=0;
+        do {
+            Registrator registrator = new RegistratorImpl();
+            User user = registrator.registerUser();
+            String LOGS = "C:\\Users\\artem\\IdeaProjects\\Party\\src\\main\\resources\\"+user.getName()+"_log.txt";//логи
+            PrintWriter printWriterLOGS = new PrintWriter(new FileOutputStream(LOGS));
+            printWriterLOGS.printf("Регистратция пройдена. Добавлено имя пользователя: "+ user.getName()+
+                    ". Введен счет пользователя: "+user.scoreOfGame()+". Введен максимальный счет теста: "+user.maxScore());
+            Game game = new GameImpl(user);
+            printWriterLOGS.printf("\nЗагружена игра.\n");//логи
+            int exit=0;
+            System.out.println("Добро пожаловать на коктейльную пати в честь вашего устройства на работу.\n" +
+                    "Вы в комнате, перед Вами выбор куда подойти. " +
+                    "Так же Вы можете пройти мимо или отдохнуть.\n"); // главное меню
+            // проверка на наличие пользователя с таким именем в базе данных
+            String url = "jdbc:postgresql://localhost:5432/userscore";
+                    String userBD="postgres";
+                    String passwordDb="artem";
+            boolean search=false;
+            int oldUserId=0;
+            String usernameCheck="";
+            String queryForSearch="select user_id, username, score, scoreMax from accountsUserScore";
+            try (Connection connectionForSearch= DriverManager.getConnection(url, userBD, passwordDb);
+                 PreparedStatement StatementForSearch = connectionForSearch.prepareStatement(queryForSearch)) {
+                ResultSet resultSet=StatementForSearch.executeQuery();
+                while (resultSet.next()){
+                    usernameCheck = resultSet.getString("username");
+                    if (usernameCheck.equals(user.getName())){ // если такой пользователь есть
+                        search=true;
+                        System.out.println("Такой пользователь есть, " +
+                                "либо выберете другое имя, нажав клавишу 8, либо выберете загрузку сохраненной игры, " +
+                                "нажав клавишу 7"); //развилка на выход
+                        Scanner scannerOfLoad = new Scanner(System.in); // взаимодействие с пользователем
+                        load=scannerOfLoad.nextInt();
+                        if (load==7) { //загрузка параметров существующего пользователя
+                            oldUserId = resultSet.getInt("user_id");
+                            System.out.println("oldUserId = "+oldUserId);
+                            System.out.println("score = "+resultSet.getInt("score"));
+                            System.out.println("scoreMax = "+resultSet.getInt("scoreMax"));
+                            if (user.scoreOfGame() > 0) {
+                                user.increaseScore(resultSet.getInt("score"));
+                            } else if (user.scoreOfGame() < 0) {
+                                user.reduceScore(resultSet.getInt("score"));
+                            }
+                            if (user.maxScore() > 0) {
+                                user.increaseMaxScore(resultSet.getInt("scoreMax"));
+                            }
+                            System.out.println("score = "+user.scoreOfGame());
+                            System.out.println("scoreMax = "+user.maxScore());
+                        }
+                    }
+                    else {load=0;}
+                }
+            }
+            catch (SQLException excpt){
+                throw new RuntimeException(excpt);
+            }
+        } while (load==8);
 
         //  сама игра
         do {
@@ -93,10 +136,6 @@ public class Application {
 
         // запись имени и счета  игрока в базу данных
 
-        String url = "jdbc:postgresql://localhost:5432/userscore";
-        String userBD="postgres";
-        String passwordDb="artem";
-
         // запаска для создание базы
         //try (Connection connectionForCreateTable= DriverManager.getConnection(url, userBD, passwordDb);
               // Statement statement = connectionForCreateTable.createStatement()) {
@@ -112,6 +151,18 @@ public class Application {
            // throw new RuntimeException(excpt);
         //}
 
+        if (search){ // если пользователь уже есть
+            // удаление записи пользователя
+            String queryForDel="DELETE from accountsUserScore where user_id=oldUserId";
+            try (Connection connectionForDel= DriverManager.getConnection(url, userBD, passwordDb);
+                 PreparedStatement StatementForDel = connectionForDel.prepareStatement(queryForDel)) {
+                ResultSet resultSet=StatementForDel.executeQuery();
+                while (resultSet.next()){StatementForDel.execute();}
+            } catch (SQLException excpt){
+                throw new RuntimeException(excpt);
+            }
+        }
+
         // подгрузка последнего id для увеличение его значение на 1
         int userid=0;
         String queryForId="select user_id from accountsUserScore";
@@ -120,7 +171,7 @@ public class Application {
             //preparedStatement.setInt(1,2);
             ResultSet resultSet=StatementForId.executeQuery();
             while (resultSet.next()){
-            userid = resultSet.getInt("user_id");
+                userid = resultSet.getInt("user_id");
             }
         }
         catch (SQLException excpt){
@@ -128,19 +179,18 @@ public class Application {
         }
 
         // запись новой строки - данных нового пользователя
-        String query="INSERT INTO accountsUserScore(user_id, username, score, scoreMax) VALUES (?, ?, ?, ?)";
-        try (Connection connectionForCreateData= DriverManager.getConnection(url, userBD, passwordDb);
-             PreparedStatement preparedStatement = connectionForCreateData.prepareStatement(query))
-        {
-            preparedStatement.setInt(1, userid+1);
-            preparedStatement.setString(2, user.getName());
-            preparedStatement.setInt(3, user.scoreOfGame());
-            preparedStatement.setInt(4, user.maxScore());
-            preparedStatement.execute();
+        String query = "INSERT INTO accountsUserScore(user_id, username, score, scoreMax) VALUES (?, ?, ?, ?)";
+        try (Connection connectionForCreateData = DriverManager.getConnection(url, userBD, passwordDb);
+                 PreparedStatement preparedStatement = connectionForCreateData.prepareStatement(query)) {
+                preparedStatement.setInt(1, userid + 1);
+                preparedStatement.setString(2, user.getName());
+                preparedStatement.setInt(3, user.scoreOfGame());
+                preparedStatement.setInt(4, user.maxScore());
+                preparedStatement.execute();
+        } catch (SQLException excpt) {
+                throw new RuntimeException(excpt);
         }
-        catch (SQLException excpt){
-            throw new RuntimeException(excpt);
-        }
+
 
         // отображение базы данных
         String queryForTest="select * from accountsUserScore";
@@ -150,10 +200,10 @@ public class Application {
             ResultSet resultSet=StatementForTest.executeQuery();
             while (resultSet.next()){
                 int user_id = resultSet.getInt("user_id");
-                String username=resultSet.getString("username");
+                String usernamenew=resultSet.getString("username");
                 int score = resultSet.getInt("score");
                 int scoreMax = resultSet.getInt("scoreMax");
-                System.out.println("Пациент № " + user_id +". "+ username +" имеет "+ score +
+                System.out.println("Пациент № " + user_id +". "+ usernamenew +" имеет "+ score +
                         " баллов из "+ scoreMax+" социальной адаптации.");
             }
         } catch (SQLException excpt){
